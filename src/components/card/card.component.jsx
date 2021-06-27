1f3b74cce580f36d6479a291815ace33cb5eaae3
Lucas Okafor <lucas.c.okafor@outlook.com>
2021-06-27 13:47:08 +0100

    deploy app

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,10 @@ import './card.styles.css';
 export const Card = (props) => {
     return (
         <div className="card-container">
-            <img alt="user" src={`https://robohash.org/${props.user.id}?set=set2&size=180x180`}/>
+            <img 
+            className="user__img" 
+            alt="user" 
+            src={`https://images.pexels.com/photos/4355346/pexels-photo-4355346.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=80&w=80`} />
             <h1>{props.user.name}</h1>
             <p>{props.user.email}</p>
         </div>
